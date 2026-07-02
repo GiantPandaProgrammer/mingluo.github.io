c7582073b86c9562513c940a072cf4c356ca0efe
Ming <ming@Mings-MacBook-Pro.local>
2026-07-01 23:24:26 -0400

    home component

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,13 @@ import Typewriter from "typewriter-effect";
 import Particles from "react-tsparticles";
 import Particle from './Particle';
 import dragon from './assets/dragon.png'; 
+import Home from './components/home.jsx';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
 
 /*
   type writer effect
@@ -21,80 +28,13 @@ function App() {
 
   return (
     <>
-      <section id="center" className="dark-mode">
-        <Particle />
-        <Container>
-          <Row>
-            <Col md={7}>
-              <h1 style={{ textAlign: "left",paddingBottom: 15, fontSize: 50 }}>
-                Hi, I'm Ming Luo
-              </h1>
-              <div style={{ textAlign: "left" }}>
-                <Typewriter
-                  options={{
-                    strings: [
-                      "Software Developer",
-                      "Volunteer",
-                      "Hobbyist Musician"
-                    ],
-                    autoStart: true,
-                    loop: true,
-                    deleteSpeed: 50,
-                    wrapperClassName: 'typewriter-text'
-                  }}
-                  textStyle={{
-                    fontSize: '48px', 
-                    color: '#ff5722',
-                    fontWeight: 'bold'
-                  }}>
-                  </Typewriter>
-                </div>
-            </Col>
-            <Col md={5}>
-              <img
-                src={me}
-                style={{ maxHeight: "450px", index: 1201 }}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col md={7}>
-              <ul className="home-about-social-links">
-                <li className="social-icons">
-                  <a
-                    href="https://github.com/GiantPandaProgrammer"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="icon-colour  home-social-icons"
-                  >
-                    <AiFillGithub size={30} color="#e4e4e4"/>
-                  </a>
-                </li>
-                <li className="social-icons">
-                  <a
-                    href="https://www.linkedin.com/in/ming-luo-94286325/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="icon-colour  home-social-icons"
-                  >
-                    <FaLinkedinIn size={30} color="#e4e4e4"/>
-                  </a>
-                </li>
-                <li className="social-icons">
-                  <a
-                    href="./mygame.html"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="icon-colour  home-social-icons"
-                  >
-                    <img src={dragon} alt="Dragon" style={{ width: '32px', height: '32px' }} />
-                  </a>
-                </li>
-              </ul>
-            </Col>
-          </Row>
-        </Container>
-      </section>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Home />} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+      </Router>
     </>
   )
 }
