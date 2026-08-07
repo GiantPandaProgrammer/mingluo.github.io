081f2ce5d2cd096f2e62795702bd816205e57ee0
Ming <ming@Mings-MacBook-Pro.local>
2026-08-07 18:45:54 -0400

    update typewriter

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -34,7 +34,7 @@ function Home() {
                   options={{
                     strings: [
                       "Coder",
-                      "Gamer Maker",
+                      "Game Maker",
                       "Volunteer",
                       "Musician",
                       "Cashier",
